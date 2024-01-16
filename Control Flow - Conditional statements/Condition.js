eb40c2315e8ceafb40c895f88fd3572100909d04
Hassan Mahamud <146654419+Hassan-mahamud@users.noreply.github.com>
2024-01-16 22:56:01 +0300

    Create Condition.js

---
+++ Control Flow - Conditional statements/Condition.js
@@ -0,0 +1,13 @@
+let greeting = "Hello";
+let name = "John";
+let age = 25;
+let isStudent = true;
+
+// Output
+console.log(`${greeting}, my name is ${name}. I am ${age} years old.`);
+
+if (isStudent) {
+  console.log("I am currently a student.");
+} else {
+  console.log("I am not a student.");
+}
