0f10665e7b5d112d58e38e5490ffdc4f057d62ee
Hassan Mahamud <146654419+Hassan-mahamud@users.noreply.github.com>
2024-01-24 17:38:16 +0300

    Create Even-odd.js

---
+++ Control Flow - Conditional statements/Even-odd.js
@@ -0,0 +1,8 @@
+// Example: Checking if a number is even or odd
+const number = 7;
+
+if (number % 2 === 0) {
+  console.log(`${number} is an even number.`);
+} else {
+  console.log(`${number} is an odd number.`);
+}
